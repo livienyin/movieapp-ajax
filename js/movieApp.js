4690f2afb9d5341336df4aa9d668d905bd3ee019
Livien Yin <LivienYin@gmail.com>
2013-05-01 23:24:14 -0700

    returns search results and makes API call for movie detail

--- js/movieApp.js
+++ js/movieApp.js
@@ -1,14 +1,68 @@
+function getMovieData(title) {
+  $.ajax({
+    url: 'http://www.omdbapi.com/?t=' + encodeURIComponent(title),
+    method: 'get',
+    dataType: 'json',
+    success: function(movie){
+      createDetailElement(movie)
+    }
+  });    
+}
+
+function createDetailElement(movie){
+  var title = movie.Title;
+  var year = movie.Year;
+  var headerElement = $('<h2 />');
+  headerElement.append(title + ' ' + year);
+  headerElement.appendTo('#movie_data')
+}
+
+function createListItem(movieData) {
+  var listElement = $('<li />');
+  listElement.attr('class', 'searchItem');
+  listElement.append(movieData.Title);
+  listElement.on('click', function (event) { getMovieData(movieData.Title); })
+  $('#results').append(listElement);
+  listElement.hide().fadeIn();
+}
+
+function createListItems(searchResults) {
+  $('#results').html('');
+  if (!searchResults.Search) {return;}
+  for (var i=0; i<searchResults.Search.length; i++) {
+    createListItem(searchResults.Search[i])
+  }
+}
+
 $(document).ready(function(){
-	$('form').on('submit', function(event){
+
+  $('#title_search').keyup(function(){
+    var titleValue = $('#title_search').val();
+    if (titleValue !== '') {
+      $.ajax({
+        url: 'http://www.omdbapi.com/?s=' + encodeURIComponent(titleValue),
+        method: 'get',
+        dataType: 'json',
+        success: function(searchResults){
+          createListItems(searchResults);
+        }
+      });
+    } else {
+      $('#results').html('');
+    }
+    return false;
+  });
+
+  $('form').on('submit', function(event){
     event.preventDefault();
+    var titleValue = $('#title_search').val();
     $.ajax({
-      url: 'http://www.omdbapi.com/?t=True%20Grit&y=1969',
+      url: 'http://www.omdbapi.com/?s=' + encodeURIComponent(titleValue),
       method: 'get',
-      // data: {todo_item: item},
       dataType: 'jsonp',
-			success: function(movie){
-     		console.log(movie)
-			}
+      success: function(movie){
+        console.log(movie);
+      }
     });
   });
 });
